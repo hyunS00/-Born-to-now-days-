e97b1d4a05bcaca9108283cb9320c4921e979b7a
hyunS00 <optifun72@gmail.com>
2024-02-26 13:24:33 +0900

    계산 프로그램

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Calendar from "react-calendar";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const now = new Date();
+  const [birthDay, setBirthDay] = useState(now);
+  const [year, setYear] = useState(0);
+  const [month, setMonth] = useState(0);
+  const [day, setDay] = useState(0);
+
+  const onChange = (birthDay) => {
+    setBirthDay(birthDay);
+    calculateDifference(birthDay);
+  };
+
+  const calculateDifference = (birthDay) => {
+    const diffTime = now - birthDay;
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+    const diffMonths = Math.floor(diffDays / 30.44);
+    const diffYears = Math.floor(diffDays / 365.25);
+    setYear(diffYears);
+    setMonth(diffMonths);
+    setDay(diffDays);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>내가 태어난 날은</h1>
+      <h2> {birthDay.toLocaleDateString()}</h2>
+      <Calendar onChange={onChange} value={birthDay} />
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h1>내가 태어난지</h1>
+        <h1>{year}년</h1>
+        <h1>{month}개월</h1>
+        <h1>{day}일</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
